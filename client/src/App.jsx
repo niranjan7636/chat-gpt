800ce8d1fda4837ea186780199a4e3fc8f1abb64
Niranjan <niranjansaini504@gmail.com>
2023-04-01 03:17:33 +0530

    url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
   } , [posts])
 
 const fetchBotResponse = async()=>{
-    const {data} = await axios.post("http://localhost:4000" , {input},
+    const {data} = await axios.post("https://chatgpt-app-bnww.onrender.com" , {input},
     { headers : {"Content-Type" : "application/json"}}
     );
     return data;
